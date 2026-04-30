89d17dbfea2dc01d9e3e1677f0cb206681ad5199
Scott Milliorn <scottmilliorn@gmail.com>
2026-04-29 20:57:33 -0700

    feat: replace Placeholder with UvAlertApp in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uvalert/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: Placeholder()));
+  runApp(const ProviderScope(child: UvAlertApp()));
 }
