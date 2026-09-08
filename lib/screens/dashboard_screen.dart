f0b3f5b3cd9a9df8436580a45bcab53c350f9edc
Scott Milliorn <scottmilliorn@gmail.com>
2026-09-07 18:21:06 -0700

    fix(dashboard): add import for ProxyErrorBanner widget

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -10,6 +10,7 @@ import 'package:uvalert/screens/settings_screen.dart';
 import 'package:uvalert/widgets/dashboard_footer.dart';
 import 'package:uvalert/widgets/dashboard_hero.dart';
 import 'package:uvalert/widgets/dashboard_no_data_view.dart';
+import 'package:uvalert/widgets/proxy_error_banner.dart';
 import 'package:uvalert/widgets/weather_alert_banner.dart';
 
 /// The main screen shown after onboarding completes.
